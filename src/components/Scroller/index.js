d5edd4c65338849543a0432a810f860eb40780ec
张子睿 <zirui.zhang@qunar.com>
2019-03-27 21:44:44 +0800

    feat: 更改scroller 的滚动

--- src/components/Scroller/index.js
+++ src/components/Scroller/index.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import {getNewObj, getAngle, getStylePre} from '../../util/scrollUtil';
+import {Animate, Ease} from '../../util/animate';
 
-const LIMIT_VERTICAL_ANGLE = 60; // 上下滑动的最小角度
-const LIMIT_HORIZONTAL_ANGLE = 30; // 左右滑动的最大角度
+const LIMIT_VERTICAL_ANGLE = 60; // 上下滑动的最大角度
+const LIMIT_HORIZONTAL_ANGLE = 30; // 左右滑动的最小角度
 
 export default class Scroller extends Component {
     constructor() {
@@ -13,7 +14,11 @@ export default class Scroller extends Component {
 
     componentWillMount() {
         this._resetProps();
-
+        this.animate = new Animate({
+            ease: Ease.circular.fn,
+            duration: 300,
+            animateFn: () => {}
+        });
     }
 
     componentDidMount() {
@@ -21,8 +26,10 @@ export default class Scroller extends Component {
             scrollHeight: this.scrollerDom.offsetHeight,
             scrollWidth: this.scrollerDom.offsetWidth,
             contentHeight: this.contentWrapDom.offsetHeight,
-            contentWidth: this.contentWrapDom.offsetWidth
+            contentWidth: this.contentWrapDom.offsetWidth,
+            maxScroll: this.props.horizontal ? this.contentWrapDom.offsetWidth - this.scrollerDom.offsetWidth : this.contentWrapDom.offsetHeight - this.scrollerDom.offsetHeight
         };
+        console.log('maxScroll:', this.contentWrapDom.offsetHeight - this.scrollerDom.offsetHeight);
     }
 
     _resetProps() {
@@ -42,6 +49,7 @@ export default class Scroller extends Component {
         }
         this.x = 0;
         this.y = 0;
+        this._delta = 0; // 防止界面跳变，缓存非边界状态下的delta
     }
 
 
@@ -63,29 +71,152 @@ export default class Scroller extends Component {
         const distanceX = Math.abs(deltaX);
         const distanceY = Math.abs(deltaY);
 
+
         this.angle = getAngle(distanceY, distanceX);
-        if (distanceX < 10 && distanceY < 10) {
+        if (distanceX < 10 && distanceY < 10) { // 距离太短不形成触摸条件
             return;
         }
+
+        if (this.isAnimate) {
+            this.animate.abort();
+        }
+
         if (!props.horizontal && this.angle <= LIMIT_VERTICAL_ANGLE) {
-            this._move(this.x + deltaX, this.y + deltaY, deltaY);
+            this._move(deltaY);
         } else if (props.horizontal && this.angle >= LIMIT_HORIZONTAL_ANGLE) {
-            this._move(this.x + deltaX, this.y + deltaY, deltaX);
+            this._move(deltaX);
         }
     }
 
     onTouchEnd = (e) => {
+        console.log('=========touchEnd ');
         this._prevent(e);
+
         this.x = this.endX;
         this.y = this.endY;
+        this._delta = 0;
+        // console.log(this.y);
+        if (this.outOfRange && this.props.bounce) {
+            this.outOfRange = false;
+            if (this.horizontal) {
+                this.scrollTo({
+                    x: this.outOfRangeInfo.val
+                });
+            } else {
+                this.scrollTo({
+                    y: this.outOfRangeInfo.val
+                });
+            }
+        }
+    }
+
+    _move(delta) {
+        console.log('delta:', delta);
+        const {horizontal, bounce} = this.props;
+        let disX;
+        let disY;
+        if (horizontal) {
+            disY = 0;
+            disX = this._getDis(this.x, delta);
+        } else {
+            disX = 0;
+            disY = this._getDis(this.y, delta);
+        }
+
+        this.endX = disX;
+        this.endY = disY;
+        if (this.isTranslate) {
+            this._translate(disX, disY);
+        } else {
+            // 使用absolute
+        }
+    }
+
+    _getDis(val, delta) {
+        const {bounce} = this.props;
+        const {maxScroll} = this.scrollInfo;
+        let res = val;
+
+        if (val + delta > 0) { // 拉到上/左边界
+            console.log('condition 1');
+            if (!bounce) {
+                res = 0;
+            } else {
+                res = delta > 0 ? val + this._delta + (delta - this._delta) / 3 : val + delta;
+                this.outOfRange = true;
+                this.outOfRangeInfo = {
+                    val: 0
+                };
+            }
+        } else if (val + delta < -maxScroll) { // 拉到下/右边界
+            console.log('condition 2');
+            if (!bounce) {
+                res = -maxScroll;
+            } else {
+                res = delta < 0 ? val + this._delta + (delta - this._delta) / 3 : val + delta;
+                this.outOfRange = true;
+                this.outOfRangeInfo = {
+                    val: -maxScroll
+                };
+            }
+        } else {
+            console.log('condition 3');
+            res = val + delta;
+            this._delta = delta;
+        }
+
+        return res;
+    }
+
+    _translate(x, y) {
+        if (typeof y === 'undefined') {
+            y = this.horizontal ? 0 : x;
+            x = this.horizontal ? x : 0;
+        }
+        this.contentWrapStyle = getNewObj(this.contentWrapStyle, {
+            [this.transformKey]: `translate3d(${x}px, ${y}px, 0px)`
+        });
+        this._setStyle(this.contentWrapDom, this.contentWrapStyle);
+    }
+
+    _setStyle(dom, style) {
+        Object.keys(style).forEach((key) => {
+            dom.style[key] = style[key];
+        });
     }
 
     getRef = (node) => {
         this.scrollerDom = node;
     }
 
-    scrollTo({x, y}, duration, easing) {
-
+    scrollTo({x, y}, duration = 500, ease = Ease.circular.fn) {
+        let val = y;
+        let startVal = this.y;
+        if (this.horizontal) {
+            val = x;
+            startVal = this.x;
+        }
+        this.isAnimate = true;
+        this.animate
+            .reset({
+                animateFn: (c) => {
+                    const dis = (val - startVal) * c + startVal;
+                    this._translate(dis);
+                    if (this.horizontal) { // 保证在动画过程中随时更新 this.x 和 this.y 的值
+                        this.x = dis;
+                        this.y = y;
+                    } else {
+                        this.x = x;
+                        this.y = y;
+                    }
+                },
+                duration,
+                ease
+            })
+            .begin()
+            .then(() => {
+                this.isAnimate = false; // 动画结束
+            });
     }
 
     _prevent(e) {
@@ -97,42 +228,6 @@ export default class Scroller extends Component {
         if (stopPropagation) {
             e.stopPropagation();
         }
-    }
-
-    _move = (x, y) => {
-        const {scrollHeight, scrollWidth, contentHeight, contentWidth} = this.scrollInfo;
-        const {horizontal, bounce} = this.props;
-        let delta = 0;
-        if (horizontal) {
-            delta = bounce ? x / 3 : 0;
-            y = 0;
-            x = x > 0 ? 0 + delta : Math.min(x, scrollWidth - contentWidth - delta);
-        } else {
-            delta = bounce ? y / 3 : 0;
-            x = 0;
-            y = y > 0 ? 0 + delta : Math.max(y, scrollHeight - contentHeight - delta);
-        }
-
-        this.endX = x;
-        this.endY = y;
-        if (this.isTranslate) {
-            this._translate(x, y);
-        } else {
-            // 使用absolute
-        }
-    }
-
-    _translate(x, y) {
-        this.contentWrapStyle = getNewObj(this.contentWrapStyle, {
-            [this.transformKey]: `translate3d(${x}px, ${y}px, 0px)`
-        });
-        this._setStyle(this.contentWrapDom, this.contentWrapStyle);
-    }
-
-    _setStyle(dom, style) {
-        Object.keys(style).forEach((key) => {
-            dom.style[key] = style[key];
-        });
     }
 
     render() {
--- src/util/animate.js
+++ src/util/animate.js
@@ -1,5 +1,6 @@
-export default class Animate {
+export class Animate {
     constructor(config) {
+        this.config = config;
         this._resetConfig(config);
     }
 
@@ -13,7 +14,6 @@ export default class Animate {
         this.ease = ease;
         this.duration = duration;
         this.animateFn = animateFn;
-        this.abort = false;
     }
 
     _getTime() {
@@ -23,24 +23,102 @@ export default class Animate {
     begin() {
         const startTime = Date.now();
         const {duration, ease, animateFn, abort} = this;
-        const step = new Promise(function _step(resolve, reject) {
-            const currentTime = Date.now();
-            let _c = ease((currentTime - startTime) / duration);
-            if (_c >= 1) {
-                resolve();
-            }
-            if (abort) {
-                this.abort = false;
-                reject();
-            }
-            animateFn && animateFn();
-            requestAnimationFrame(_step);
+        let aniId = 0;
+        return new Promise((resolve, reject) => {
+            function step() {
+                const currentTime = Date.now();
+                let _c = ease((currentTime - startTime) / duration);
+                if (_c >= 1) {
+                    animateFn(1);
+                    resolve();
+                    return;
+                }
+                animateFn && animateFn(_c);
+                aniId = requestAnimationFrame(step);
+            };
+            this.abort = () => {
+                cancelAnimationFrame(aniId);
+                animateFn && animateFn(0);
+                reject('cancel animate');
+            };
+            step();
         });
-
-        step();
     }
 
-    abort() {
-        this.abort = true;
+    reset(config) {
+        this.config = {
+            ...this.config,
+            ...config
+        };
+        this._resetConfig(this.config);
+        return this;
     }
 }
+
+export const Ease = {
+    quadratic: {
+        style: 'cubic-bezier(0.25, 0.46, 0.45, 0.94)',
+        fn: function(k) {
+            return k * (2 - k);
+        }
+    },
+    circular: {
+        style: 'cubic-bezier(0.1, 0.57, 0.1, 1)',	// Not properly "circular" but this looks better, it should be (0.075, 0.82, 0.165, 1)
+        fn: function(k) {
+            if(k > 1) {
+                k = 1;
+            }
+            let _k = k - 1;
+
+            return Math.sqrt(1 - (_k * _k));
+        }
+    },
+    back: {
+        style: 'cubic-bezier(0.175, 0.885, 0.32, 1.275)',
+        fn: function(k) {
+            let b = 4;
+            let _k = k - 1;
+
+            return _k * _k * ((b + 1) * _k + b) + 1;
+        }
+    },
+    bounce: {
+        style: '',
+        fn: function(k) {
+            let y;
+            let _k = k;
+
+            if ((_k / 1) < (1 / 2.75)) {
+                _k = _k / 1;
+                y = 7.5625 * _k * _k;
+            } else if (k < (2 / 2.75)) {
+                _k -= (1.5 / 2.75);
+                y = 7.5625 * _k * _k + 0.75;
+            } else if (k < (2.5 / 2.75)) {
+                _k -= (2.25 / 2.75);
+                y = 7.5625 * _k * _k + 0.9375;
+            } else {
+                _k -= (2.625 / 2.75);
+                y = 7.5625 * _k * _k + 0.984375;
+            }
+
+            return y;
+        }
+    },
+    elastic: {
+        style: '',
+        fn: function(k) {
+            let f = 0.22;
+            let e = 0.4;
+
+            if (k === 0) {
+                return 0;
+            }
+            if (k === 1) {
+                return 1;
+            }
+
+            return (e * Math.pow(2, -10 * k) * Math.sin((k - f / 4) * (2 * Math.PI) / f) + 1);
+        }
+    }
+};
